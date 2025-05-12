f1605fd4fcc4ef5c085acf7b7072ffaab5caf5ac
Alexander Boyd <alexanderboydjm@gmail.com>
2025-05-12 11:55:21 -0400

    centering about me title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ export default function About() {
   return (
     <div className="min-h-screen bg-primary-1 flex flex-col items-center justify-center px-4">
       <main className="space-y-16 px-4">
-        <div className="w-full flex items-center justify-end h-[60vh] bg-outlaw">
-        <div className="text-right mr-32">
+        <div className="w-full flex items-center justify-center h-[60vh] bg-outlaw">
+        <div className="text-center">
         <h3 className="text-primary-2 tracking-wider text-center">This is the part where I</h3>
         <h1 className="text-primary-2 font-freckle tracking-wider text-center">Talk About Myself</h1>
         </div>
