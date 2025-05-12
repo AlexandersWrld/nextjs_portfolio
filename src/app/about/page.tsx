d1f8c64899b3fdedd87abb64e301b4de991551d7
Alexander Boyd <alexanderboydjm@gmail.com>
2025-05-12 03:02:24 -0400

    about me attempted fix 1

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="h-screen bg-primary-1">
+    <div className="min-h-screen bg-primary-1 flex flex-col items-center justify-center px-4">
       <main className="space-y-16 px-4">
-        <div className="w-full flex items-center justify-end px-8 h-[60vh] bg-outlaw">
+        <div className="w-full flex items-center justify-end h-[60vh] bg-outlaw">
         <div className="text-right mr-32">
         <h1 className="text-primary-2 text-8xl font-freckle tracking-wider text-center">Meet The Man with The Method</h1>
         </div>
         </div>
         
         <div className="text-left ml-12 mr-12 space-y-8 leading-12">
-        <p className="text-3xl text-primary-2 max-w-8xl"> I got my start as a graphic designer. I was fresh out of highschool back in Jamaica, with little idea about what I wanted to do next.
+        <p className="text-xl text-primary-2 max-w-8xl"> I got my start as a graphic designer. I was fresh out of highschool back in Jamaica, with little idea about what I wanted to do next.
           I always had a love for art and I used to make lot of prints on my &#40;totally not pirated&#41; copy of photoshop. One day I designed an advert my family was looking to run in our local newspaper and the paper company 
           was so impressed that they called back and offered me a full-time design role. Once my design career started it was only a matter of time before I found my way to frontend, as all designers eventually do.</p>
-          <p className="text-3xl text-primary-2 max-w-8xl">
+          <p className="text-xl text-primary-2 max-w-8xl">
           I am familiar with popular frontend frameworks like Next.js, Node.js and React. I also have advanced proficiency in Adobe Photoshop, Illustrator and After Effects.
           </p>
         
